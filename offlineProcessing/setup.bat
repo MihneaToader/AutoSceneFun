956329764cdbb42e69a4036d94d49ffdb145906a
Axel2017 <axelmauricewagner@mac.com>
2024-06-17 15:35:55 +0200

    windows adaptation

--- offlineProcessing/setup.bat
+++ offlineProcessing/setup.bat
@@ -84,6 +84,8 @@ if "%DOWNLOAD_HEAVY%"=="true" (
     echo Downloading Pose landmarker heavy...
     curl -O https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy/float16/latest/pose_landmarker_heavy.task
 )
+call conda activate 3dv
+python -m pip install git+https://github.com/m-bain/whisperx.git
 
 echo Download completed.
 
